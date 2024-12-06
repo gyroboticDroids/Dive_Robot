d55e203bfdd23a6dee1f8248a6ba4a49d7be92e3
NaeNae4321 <debruler.nl@gmail.com>
2024-12-06 16:57:26 -0500

    Worked on TestMotors and added Hardware class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Hardware {
+    public DcMotor frontLeft;
+    public DcMotor frontRight;
+    public DcMotor rearLeft;
+    public DcMotor rearRight;
+
+    public DcMotor vertSlide1;
+    public DcMotor vertSlide2;
+
+    public DcMotor horizontalSlide;
+
+    public Servo specimenExtension;
+
+    public Servo intake;
+    public Servo outake;
+
+    public CRServo hangLeft;
+    public CRServo hangRight;
+
+    public void ConfigureHardware(HardwareMap hardwareMap)
+        {
+            frontLeft = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+            frontRight = hardwareMap.get(DcMotor.class, "frontRightMotor");
+            rearLeft = hardwareMap.get(DcMotor.class, "rearLeftMotor");
+            rearRight = hardwareMap.get(DcMotor.class, "rearRightMotor");
+
+            vertSlide1 = hardwareMap.get(DcMotor.class, "vertSlide1");
+            vertSlide2 = hardwareMap.get(DcMotor.class, "vertSlide2");
+
+            horizontalSlide = hardwareMap.get(DcMotor.class, "horizontalSlide");
+
+            specimenExtension = hardwareMap.get(Servo.class, "extension");
+
+            outake = hardwareMap.get(Servo.class, "outake");
+            intake = hardwareMap.get(Servo.class, "intake");
+
+            frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            rearLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            rearRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMotors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMotors.java
@@ -2,55 +2,17 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.AnalogInput;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "TestMotors")
 public class TestMotors extends OpMode {
-    private DcMotor frontLeft;
-    private DcMotor frontRight;
-    private DcMotor rearLeft;
-    private DcMotor rearRight;
-
-    private DcMotor vertSlide1;
-    private DcMotor vertSlide2;
-
-    private DcMotor horizontalSlide;
-
-    private Servo specimenExtension;
-
-    private Servo intake;
-    private Servo outake;
-
-    private CRServo hangLeft;
-    private CRServo hangRight;
+    private Hardware hardware;
 
     float extensionControls = 0;
 
     @Override
     public void init()
     {
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeftMotor");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRightMotor");
-        rearLeft = hardwareMap.get(DcMotor.class, "rearLeftMotor");
-        rearRight = hardwareMap.get(DcMotor.class, "rearRightMotor");
-
-        vertSlide1 = hardwareMap.get(DcMotor.class, "vertSlide1");
-        vertSlide2 = hardwareMap.get(DcMotor.class, "vertSlide2");
-
-        horizontalSlide = hardwareMap.get(DcMotor.class, "horizontalSlide");
-
-        specimenExtension = hardwareMap.get(Servo.class, "extension");
-
-        outake = hardwareMap.get(Servo.class, "outake");
-        intake = hardwareMap.get(Servo.class, "intake");
-
-        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rearLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rearRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        hardware.ConfigureHardware(hardwareMap);
     }
 
     @Override
@@ -62,55 +24,55 @@ public class TestMotors extends OpMode {
 
         if(gamepad1.a)
         {
-            frontLeft.setPower(gamepad1.left_stick_y);
+            hardware.frontLeft.setPower(gamepad1.left_stick_y);
         }
         else
         {
-            frontLeft.setPower(0);
+            hardware.frontLeft.setPower(0);
         }
 
         if(gamepad1.b)
         {
-            frontRight.setPower(gamepad1.left_stick_y);
+            hardware.frontRight.setPower(gamepad1.left_stick_y);
         }
         else
         {
-            frontRight.setPower(0);
+            hardware.frontRight.setPower(0);
         }
 
         if(gamepad1.x)
         {
-            rearLeft.setPower(gamepad1.left_stick_y);
+            hardware.rearLeft.setPower(gamepad1.left_stick_y);
         }
         else
         {
-            rearLeft.setPower(0);
+            hardware.rearLeft.setPower(0);
         }
 
         if(gamepad1.y)
         {
-            rearRight.setPower(gamepad1.left_stick_y);
+            hardware.rearRight.setPower(gamepad1.left_stick_y);
         }
         else
         {
-            rearRight.setPower(0);
+            hardware.rearRight.setPower(0);
         }
 
-        vertSlide1.setPower(gamepad1.right_stick_y);
-        vertSlide2.setPower(-gamepad1.right_stick_y);
+        hardware.vertSlide1.setPower(gamepad1.right_stick_y);
+        hardware.vertSlide2.setPower(-gamepad1.right_stick_y);
 
-        horizontalSlide.setPower(gamepad1.left_stick_x);
+        hardware.horizontalSlide.setPower(gamepad1.left_stick_x);
 
         extensionControls += (gamepad1.right_trigger - gamepad1.left_trigger) / 100;
         extensionControls = Math.min(Math.max(extensionControls ,1), -1);
 
-        specimenExtension.setPosition(extensionControls);
+        hardware.specimenExtension.setPosition(extensionControls);
 
-        telemetry.addData("Horizontal slide pos", horizontalSlide.getCurrentPosition());
-        telemetry.addData("Vertical slide pos", vertSlide2.getCurrentPosition());
-        telemetry.addData("Extension slide pos", specimenExtension.getPosition());
-        telemetry.addData("Intake servo pos", intake.getPosition());
-        telemetry.addData("Outake servo pos", outake.getPosition());
+        telemetry.addData("Horizontal slide pos", hardware.horizontalSlide.getCurrentPosition());
+        telemetry.addData("Vertical slide pos", hardware.vertSlide2.getCurrentPosition());
+        telemetry.addData("Extension slide pos", hardware.specimenExtension.getPosition());
+        telemetry.addData("Intake servo pos", hardware.intake.getPosition());
+        telemetry.addData("Outake servo pos", hardware.outake.getPosition());
 
         telemetry.update();
     }
