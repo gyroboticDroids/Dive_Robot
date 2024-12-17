a75915c4e3ad6484dcf8783caada2381025b3959
awhol <blueshots06@gmail.com>
2024-12-17 17:45:26 -0500

    Worked vertical extension

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/VerticalExtension.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/VerticalExtension.java
@@ -6,24 +6,48 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.Constants;
 import org.firstinspires.ftc.teamcode.Hardware;
 
 public class VerticalExtension {
     private Hardware hardware;
+    private Gamepad gamepad;
 
-    public void configure (HardwareMap hardwareMap)
+    String state;
+
+    public VerticalExtension (HardwareMap hardwareMap, Gamepad gamepad2)
     {
 
-        hardware = new Hardware();
-        hardware.ConfigureHardware(hardwareMap);
+        hardware = new Hardware(hardwareMap);
+        gamepad = gamepad2;
+
+
 
     }
 
     public void Update()
     {
+        switch (state)
+        {
+            case "reset":
+                hardware.outtake.setPosition(Constants.OUTTAKE_CLOSED);
+                VertSlidesPosition(Constants.VERT_SLIDES_TRANSFER);
+                break;
+        }
+
+    }
+    public void SetState(String s)
+    {
+        state = s;
+        Update();
+    }
+
+    public void VertSlidesPosition(double position)
+    {
 
 
 
     }
+
 
 }
