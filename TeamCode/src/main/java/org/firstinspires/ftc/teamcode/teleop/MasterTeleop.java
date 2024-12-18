0428aa98d7b0cbe29ad642566ed860050f9f8a12
NaeNae4321 <debruler.nl@gmail.com>
2024-12-17 21:31:20 -0500

    Slight change to MasterTeleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MasterTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MasterTeleop.java
@@ -6,16 +6,27 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name = "Master Tele-op", group = "Tele-op")
 public class MasterTeleop extends OpMode {
     Drive drive;
+    VerticalExtension vertExtension;
 
     @Override
     public void init()
     {
         drive = new Drive(hardwareMap, gamepad1);
+        vertExtension = new VerticalExtension(hardwareMap);
     }
 
     @Override
     public void loop()
     {
         drive.Update();
+        vertExtension.Update();
+
+        if(!vertExtension.IsActive())
+        {
+            if(gamepad2.dpad_down)
+            {
+                vertExtension.SetState("reset");
+            }
+        }
     }
 }
