a59119e0ac022b620bea0726c3986db3370cd8d2
awhol <blueshots06@gmail.com>
2024-12-18 19:56:53 -0500

    Worked on outtake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Outtake.java
@@ -1,22 +1,20 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.Constants;
 import org.firstinspires.ftc.teamcode.Hardware;
+import org.firstinspires.ftc.teamcode.pedroPathing.util.Timer;
 
 public class Outtake {
-    private Hardware hardware;
-    private Gamepad gamepad;
+    private final Hardware hardware;
 
     String state;
 
-    public Outtake (HardwareMap hardwareMap, Gamepad gamepad2)
+    public Outtake (HardwareMap hardwareMap)
     {
 
         hardware = new Hardware(hardwareMap);
-        gamepad = gamepad2;
 
     }
     public void Update()
@@ -29,12 +27,46 @@ public class Outtake {
                 hardware.wrist.setPosition(Constants.WRIST_TRANSFER);
                 break;
 
-            case "specimen":
+            case "retract":
+                hardware.outtake.setPosition(Constants.OUTTAKE_OPEN);
+                hardware.pivot.setPosition(Constants.PIVOT_TRANSFER);
+                hardware.wrist.setPosition(Constants.WRIST_TRANSFER);
+                break;
+
+            case "transfer":
+                hardware.pivot.setPosition(Constants.PIVOT_TRANSFER);
+                hardware.wrist.setPosition(Constants.WRIST_TRANSFER);
+                break;
+
+            case "grip":
                 hardware.outtake.setPosition(Constants.OUTTAKE_CLOSED);
+                break;
+
+            case "score sample":
+                hardware.pivot.setPosition(Constants.PIVOT_SAMPLE);
+                hardware.wrist.setPosition(Constants.WRIST_SAMPLE);
+                break;
+
+
+            case "collect specimen":
+                hardware.pivot.setPosition(Constants.PIVOT_OFF_WALL);
+                hardware.wrist.setPosition(Constants.WRIST_OFF_WALL);
+                break;
+
+            case "transfer specimen":
                 hardware.pivot.setPosition(Constants.PIVOT_SPECIMEN);
                 hardware.wrist.setPosition(Constants.WRIST_SPECIMEN);
                 break;
+
         }
+
+
+    }
+
+    public void SetState(String s)
+    {
+        state = s;
+        Update();
     }
 
 }
