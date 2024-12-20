283fe3d676a8f0a04a1ae2c789f7b5156a7c4287
NaeNae4321 <debruler.nl@gmail.com>
2024-12-20 17:17:41 -0500

    Worked on MasterTeleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MasterTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MasterTeleop.java
@@ -19,14 +19,48 @@ public class MasterTeleop extends OpMode {
     public void loop()
     {
         drive.Update();
+        VertExtensionUpdate();
+    }
+
+    boolean isGrabbingSpecimen = false;
+    boolean isTransferringSpecimen = false;
+
+    void VertExtensionUpdate()
+    {
         vertExtension.Update();
 
-        if(!vertExtension.IsActive())
+        isTransferringSpecimen = vertExtension.state == "transfer specimen";
+
+        if(gamepad2.dpad_down && !vertExtension.IsActive())
         {
-            if(gamepad2.dpad_down)
-            {
-                vertExtension.SetState("reset");
-            }
+            vertExtension.SetState("retract");
         }
+
+        if(gamepad2.a && vertExtension.state == "reset" && !isGrabbingSpecimen && !isTransferringSpecimen)
+        {
+            vertExtension.SetState("collect specimen");
+        }
+
+        if(gamepad2.a && isGrabbingSpecimen)
+        {
+            vertExtension.SetState("grab specimen");
+        }
+
+        if(gamepad2.a && isTransferringSpecimen)
+        {
+            vertExtension.SetState("score specimen");
+        }
+
+        if(gamepad2.b && vertExtension.state == "reset")
+        {
+            vertExtension.SetState("transfer");
+        }
+
+        if(gamepad2.b && vertExtension.state == "grip")
+        {
+            vertExtension.SetState("score sample");
+        }
+
+        isGrabbingSpecimen = vertExtension.state == "collect specimen" && !gamepad2.a;
     }
 }
