c6e5f8ccc18a48108a9101a7431986a73d72db13
NaeNae4321 <debruler.nl@gmail.com>
2025-01-07 11:31:32 -0500

    worked on sample auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/AutoConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/AutoConstants.java
@@ -6,7 +6,7 @@ public class AutoConstants {
     //Poses used in SampleAuto
     public static Pose START_POSE = new Pose(7.125, 102.5, Math.toRadians(0));
     public static Pose SCORE_SAMPLE = new Pose(13, 131, Math.toRadians(-45));
-    public static Pose SAMPLE_RIGHT = new Pose(30, 121, Math.toRadians(0));
+    public static Pose SAMPLE_RIGHT = new Pose(16, 121, Math.toRadians(0));
     public static Pose SAMPLE_MIDDLE = new Pose(20, 131, Math.toRadians(0));
     public static Pose SAMPLE_LEFT = new Pose(24, 136, Math.toRadians(15));
     public static Pose PARK = new Pose(60, 94, Math.toRadians(0));
