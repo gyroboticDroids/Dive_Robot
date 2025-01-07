7e2b9529742d963d9d2ee7e7190147e4141e8456
NaeNae4321 <debruler.nl@gmail.com>
2025-01-07 11:29:08 -0500

    worked on sample auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SampleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SampleAuto.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.pedropathing.follower.Follower;
-import com.pedropathing.localization.Pose;
 import com.pedropathing.pathgen.BezierCurve;
 import com.pedropathing.pathgen.BezierLine;
 import com.pedropathing.pathgen.MathFunctions;
@@ -12,6 +11,7 @@ import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.teamcode.constants.AutoConstants;
 import org.firstinspires.ftc.teamcode.constants.IntakeConstants;
 import org.firstinspires.ftc.teamcode.constants.OuttakeConstants;
 import org.firstinspires.ftc.teamcode.teleop.Intake;
@@ -31,39 +31,32 @@ public class SampleAuto extends OpMode {
     private int actionState;
     private Path currentPath;
 
-    private final Pose startPose = new Pose(7.125, 102.5, Math.toRadians(0));
-    private final Pose scoreSample = new Pose(13, 131, Math.toRadians(-45));
-    private final Pose sampleRight = new Pose(16, 121, Math.toRadians(0));
-    private final Pose sampleMiddle = new Pose(20, 131, Math.toRadians(0));
-    private final Pose sampleLeft = new Pose(24, 136, Math.toRadians(15));
-    private final Pose park = new Pose(60, 94, Math.toRadians(0));
-
     private Path scorePreload, collectSampleRight, scoreSampleRight, collectSampleMiddle, scoreSampleMiddle, collectSampleLeft, scoreSampleLeft, touchBar;
 
     public void buildPaths() {
-        scorePreload = new Path(new BezierLine(new Point(startPose), new Point(scoreSample)));
-        scorePreload.setLinearHeadingInterpolation(startPose.getHeading(), scoreSample.getHeading());
+        scorePreload = new Path(new BezierLine(new Point(AutoConstants.START_POSE), new Point(AutoConstants.SCORE_SAMPLE)));
+        scorePreload.setLinearHeadingInterpolation(AutoConstants.START_POSE.getHeading(), AutoConstants.SCORE_SAMPLE.getHeading());
 
-        collectSampleRight = new Path(new BezierLine(new Point(scoreSample), new Point(sampleRight)));
-        collectSampleRight.setLinearHeadingInterpolation(scoreSample.getHeading(), sampleRight.getHeading());
+        collectSampleRight = new Path(new BezierLine(new Point(AutoConstants.SCORE_SAMPLE), new Point(AutoConstants.SAMPLE_RIGHT)));
+        collectSampleRight.setLinearHeadingInterpolation(AutoConstants.SCORE_SAMPLE.getHeading(), AutoConstants.SAMPLE_RIGHT.getHeading());
 
-        scoreSampleRight = new Path(new BezierLine(new Point(sampleRight), new Point(scoreSample)));
-        scoreSampleRight.setLinearHeadingInterpolation(sampleRight.getHeading(), scoreSample.getHeading());
+        scoreSampleRight = new Path(new BezierLine(new Point(AutoConstants.SAMPLE_RIGHT), new Point(AutoConstants.SCORE_SAMPLE)));
+        scoreSampleRight.setLinearHeadingInterpolation(AutoConstants.SAMPLE_RIGHT.getHeading(), AutoConstants.SCORE_SAMPLE.getHeading());
 
-        collectSampleMiddle = new Path(new BezierLine(new Point(scoreSample), new Point(sampleMiddle)));
-        collectSampleMiddle.setLinearHeadingInterpolation(scoreSample.getHeading(), sampleMiddle.getHeading());
+        collectSampleMiddle = new Path(new BezierLine(new Point(AutoConstants.SCORE_SAMPLE), new Point(AutoConstants.SAMPLE_MIDDLE)));
+        collectSampleMiddle.setLinearHeadingInterpolation(AutoConstants.SCORE_SAMPLE.getHeading(), AutoConstants.SAMPLE_MIDDLE.getHeading());
 
-        scoreSampleMiddle = new Path(new BezierLine(new Point(sampleMiddle), new Point(scoreSample)));
-        scoreSampleMiddle.setLinearHeadingInterpolation(sampleMiddle.getHeading(), scoreSample.getHeading());
+        scoreSampleMiddle = new Path(new BezierLine(new Point(AutoConstants.SAMPLE_MIDDLE), new Point(AutoConstants.SCORE_SAMPLE)));
+        scoreSampleMiddle.setLinearHeadingInterpolation(AutoConstants.SAMPLE_MIDDLE.getHeading(), AutoConstants.SCORE_SAMPLE.getHeading());
 
-        collectSampleLeft = new Path(new BezierLine(new Point(scoreSample), new Point(sampleLeft)));
-        collectSampleLeft.setLinearHeadingInterpolation(scoreSample.getHeading(), sampleLeft.getHeading());
+        collectSampleLeft = new Path(new BezierLine(new Point(AutoConstants.SCORE_SAMPLE), new Point(AutoConstants.SAMPLE_LEFT)));
+        collectSampleLeft.setLinearHeadingInterpolation(AutoConstants.SCORE_SAMPLE.getHeading(), AutoConstants.SAMPLE_LEFT.getHeading());
 
-        scoreSampleLeft = new Path(new BezierLine(new Point(sampleLeft), new Point(scoreSample)));
-        scoreSampleLeft.setLinearHeadingInterpolation(sampleLeft.getHeading(), scoreSample.getHeading());
+        scoreSampleLeft = new Path(new BezierLine(new Point(AutoConstants.SAMPLE_LEFT), new Point(AutoConstants.SCORE_SAMPLE)));
+        scoreSampleLeft.setLinearHeadingInterpolation(AutoConstants.SAMPLE_LEFT.getHeading(), AutoConstants.SCORE_SAMPLE.getHeading());
 
-        touchBar = new Path(new BezierCurve(new Point(scoreSample), new Point(park.getX(), scoreSample.getY()), new Point(park)));
-        touchBar.setLinearHeadingInterpolation(scoreSample.getHeading(), park.getHeading());
+        touchBar = new Path(new BezierCurve(new Point(AutoConstants.SCORE_SAMPLE), new Point(AutoConstants.PARK.getX(), AutoConstants.SCORE_SAMPLE.getY()), new Point(AutoConstants.PARK)));
+        touchBar.setLinearHeadingInterpolation(AutoConstants.SCORE_SAMPLE.getHeading(), AutoConstants.PARK.getHeading());
     }
 
     public void autonomousPathUpdate() {
@@ -84,11 +77,9 @@ public class SampleAuto extends OpMode {
                 - Robot Position: "if(follower.getPose().getX() > 36) {}"
                 */
 
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(robotInPos){
                     /* Score Preload */
 
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
                     if(pathTimer.getElapsedTimeSeconds() > 7) {
                         currentPath = collectSampleRight;
                         follower.followPath(currentPath, true);
@@ -97,11 +88,9 @@ public class SampleAuto extends OpMode {
                 }
                 break;
             case 2:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup1Pose's position */
                 if(robotInPos) {
                     /* Grab Sample */
 
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are scoring the sample */
                     if(pathTimer.getElapsedTimeSeconds() > 7) {
                         currentPath = scoreSampleRight;
                         follower.followPath(currentPath, true);
@@ -110,11 +99,9 @@ public class SampleAuto extends OpMode {
                 }
                 break;
             case 3:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(robotInPos) {
                     /* Score Sample */
 
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
                     if(pathTimer.getElapsedTimeSeconds() > 7) {
                         currentPath = collectSampleMiddle;
                         follower.followPath(currentPath, true);
@@ -123,11 +110,9 @@ public class SampleAuto extends OpMode {
                 }
                 break;
             case 4:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup2Pose's position */
                 if(robotInPos) {
                     /* Grab Sample */
 
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are scoring the sample */
                     if(pathTimer.getElapsedTimeSeconds() > 7) {
                         currentPath = scoreSampleMiddle;
                         follower.followPath(currentPath, true);
@@ -136,7 +121,6 @@ public class SampleAuto extends OpMode {
                 }
                 break;
             case 5:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(robotInPos) {
                     /* Score Sample */
 
@@ -149,11 +133,9 @@ public class SampleAuto extends OpMode {
                 }
                 break;
             case 6:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup3Pose's position */
                 if(robotInPos) {
                     /* Grab Sample */
 
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are scoring the sample */
                     if(pathTimer.getElapsedTimeSeconds() > 7) {
                         currentPath = scoreSampleLeft;
                         follower.followPath(currentPath, true);
@@ -162,7 +144,6 @@ public class SampleAuto extends OpMode {
                 }
                 break;
             case 7:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(robotInPos) {
                     /* Score Sample */
 
@@ -175,7 +156,6 @@ public class SampleAuto extends OpMode {
                 }
                 break;
             case 8:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(robotInPos) {
                     /* Level 1 Ascent */
 
@@ -190,7 +170,7 @@ public class SampleAuto extends OpMode {
         switch (actionState) {
             case 0:
                 outtake.setState(OuttakeConstants.SCORE_SAMPLE_READY_HIGH);
-                setActionState(1);
+                setActionState(-1);
                 break;
 
             case 1:
@@ -259,7 +239,7 @@ public class SampleAuto extends OpMode {
 
         Constants.setConstants(FConstants.class, LConstants.class);
         follower = new Follower(hardwareMap);
-        follower.setStartingPose(startPose);
+        follower.setStartingPose(AutoConstants.START_POSE);
         buildPaths();
 
         currentPath = scorePreload;
