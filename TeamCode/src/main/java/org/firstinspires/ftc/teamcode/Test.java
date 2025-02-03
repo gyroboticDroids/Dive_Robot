4c46085032126089876dd44e16cb470727f16f74
NaeNae4321 <debruler.nl@gmail.com>
2025-02-03 14:08:34 -0500

    worked on Test for servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -6,23 +6,32 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Test")
 public class Test extends OpMode {
-    Servo servo;
+    Servo servo1;
+    Servo servo2;
+    Servo servo3;
+
 
     public void init()
     {
-        servo = hardwareMap.get(Servo.class, "Servo");
+        servo1 = hardwareMap.get(Servo.class, "ch");
+        servo2 = hardwareMap.get(Servo.class, "hub0");
+        servo3 = hardwareMap.get(Servo.class, "hub1");
     }
 
     public void loop()
     {
         if (gamepad1.a){
-            servo.setPosition(0);
+            servo1.setPosition(0);
         }
         if(gamepad1.b){
-            servo.setPosition(0.5);
+            servo1.setPosition(0.5);
         }
         if(gamepad1.x){
-            servo.setPosition(1);
+            servo1.setPosition(1);
         }
+
+        servo1.setPosition(gamepad1.left_stick_y);
+        servo2.setPosition(gamepad1.left_stick_y);
+        servo3.setPosition(gamepad1.right_stick_y);
     }
 }
