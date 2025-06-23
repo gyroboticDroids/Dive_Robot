172ebad610e79b18cd493e656c8b904bf8754110
NaeNae4321 <debruler.nl@gmail.com>
2025-06-23 12:14:38 -0400

    Worked on sample auto and @disable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/FiveSpecimenOneSampleAutoPushing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/FiveSpecimenOneSampleAutoPushing.java
@@ -8,6 +8,7 @@ import com.pedropathing.pathgen.PathChain;
 import com.pedropathing.pathgen.Point;
 import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.constants.AutoConstants;
@@ -20,6 +21,7 @@ import org.firstinspires.ftc.teamcode.teleop.Outtake;
 import pedroPathing.constants.FConstants;
 import pedroPathing.constants.LConstants;
 
+@Disabled
 @Autonomous(name = "5 + 1 / 5 + 0 no preload", group = "autonomous specimens", preselectTeleOp = "Master Tele-op")
 public class FiveSpecimenOneSampleAutoPushing extends OpMode {
     private static final double SLOW_ZERO_POWER_ACCEL = 1.5;
